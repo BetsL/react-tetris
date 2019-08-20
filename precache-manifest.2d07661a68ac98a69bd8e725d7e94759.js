518932250f089a7487d974aef14be0530e2f75c9
Bets <elizabeth.lidstrom@gmail.com>
2019-08-20 12:35:05 -0400

    Updates

---
+++ precache-manifest.2d07661a68ac98a69bd8e725d7e94759.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "310a6a1cdf35b28f152a5aedad6e712b",
+    "url": "/react-tetris/index.html"
+  },
+  {
+    "revision": "52d94f0c355f272b12f9",
+    "url": "/react-tetris/static/css/main.715a6e74.chunk.css"
+  },
+  {
+    "revision": "60d8e70fda9dcfef9ce5",
+    "url": "/react-tetris/static/js/2.80244021.chunk.js"
+  },
+  {
+    "revision": "52d94f0c355f272b12f9",
+    "url": "/react-tetris/static/js/main.334c8180.chunk.js"
+  },
+  {
+    "revision": "3cbf365fbc4eeca393c0",
+    "url": "/react-tetris/static/js/runtime~main.1a1357f3.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/react-tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/react-tetris/static/media/bg.2810fe6b.png"
+  }
+]);
